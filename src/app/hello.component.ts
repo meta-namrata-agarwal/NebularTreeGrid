0d444ea8b99c30f1ed8e5218b5b078a51cf1b5ca
meta-namrata-agarwal <namrata.agarwal@metacube.com>
2020-05-16 21:27:35 +0530

    3 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,37 +1,94 @@
 import { Component } from '@angular/core';
+import { NbGetters, NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
 
-interface TreeNode<T> {
-  data: T;
-  children?: TreeNode<T>[];
-  expanded?: boolean;
+interface parentEntry {
+  key: number,
+  slotName: string,
+  slotNumber: 6,
+  slotElective: boolean,
+  slotPaperStructureDTO: FSEntry;
 }
 
 interface FSEntry {
   name: string;
-  size: string;
-  kind: string;
-  items?: number;
+  expanded?: boolean;
+  entity: string;
+  code: string;
+  elective: boolean;
+  selected: boolean;
+  slotPaperStructures?: FSEntry[];
 }
+
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  customColumn = 'name';
-  defaultColumns = [ 'size', 'kind', 'items' ];
-  allColumns = [ this.customColumn, ...this.defaultColumns ];
+export class AppComponent {
+  customColumn = 'slotname';
+  defaultColumns = ['name', 'entity', 'code', 'elective'];
+  allColumns = [this.customColumn, ...this.defaultColumns];
+   debugger; 
+  source: NbTreeGridDataSource<parentEntry>;
+   debugger; 
+  constructor(dataSourceBuilder: NbTreeGridDataSourceBuilder<parentEntry>) {
+   
+    const getters: NbGetters<parentEntry, parentEntry> = {
+      dataGetter: 
+      (node: parentEntry["slotPaperStructureDTO"]) =>
+       node,
+      childrenGetter: (node: parentEntry["slotPaperStructureDTO"]) => node.slotPaperStructureDTO.slotPaperStructures || undefined,
+      // expandedGetter: (node: FSEntry) => !!node.expanded,
+    };
+    this.source = dataSourceBuilder.create(this.datas, getters);
+  }
 
-  data: TreeNode<FSEntry>[] = [
-    {
-      data: { name: 'Projects', size: '1.8 MB', items: 5, kind: 'dir' }
-    },
-    {
-      data: { name: 'Reports', kind: 'dir', size: '400 KB', items: 2 }
-    },
-    {
-      data: { name: 'Other', kind: 'dir', size: '109 MB', items: 2 }
+  toggle(checked: boolean) {
+    checked = !checked;
+  }
+  datas: parentEntry[] = [{
+    "key": 6,
+    "slotName": "Extra Curricular Activities",
+    "slotNumber": 6,
+    "slotPaperStructureDTO": {
+      "entity": "PAPER",
+      "name": null,
+      "code": null,
+      "elective": true,
+      "slotPaperStructures": [
+        {
+          "entity": "PAPER",
+          "name": "NCC-ACTIVITY",
+          "code": "ECA-NCC",
+          "slotPaperStructures": [],
+          "elective": true
+        },
+        {
+          "entity": "PAPER",
+          "name": "NSS-ACTIVITY",
+          "code": "ECA-NSS",
+          "slotPaperStructures": [],
+          "elective": true
+        },
+        {
+          "entity": "PAPER",
+          "name": "SPORTS",
+          "code": "ECA-SPORTS",
+          "slotPaperStructures": [],
+          "elective": true
+        },
+        {
+          "entity": "PAPER",
+          "name": "CULTURAL ACTIVITY",
+          "code": "ECA-CUA",
+          "slotPaperStructures": [],
+          "elective": true
+        }
+      ],
     }
-  ];
+  }]
+
+
 }
